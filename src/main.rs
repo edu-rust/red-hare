1e6f30ba8671e3b6dc8b82598deaea5afd23dc17
DR.MA <DR.MA@DR.MA>
2025-12-21 17:24:08 +0800

    rdb刷盘机制完善,api测试案例.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,19 @@ mod core;
 mod utils;
 
 use api::routers::create_router;
-use std::string::ToString;
+use log::{error, info};
 const ADDR: &str = "127.0.0.1:7200";
 #[tokio::main]
 async fn main() {
     let app = create_router();
     // 启动服务器 (适用于 axum 0.8.8)
     let listener = tokio::net::TcpListener::bind(ADDR).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    match axum::serve(listener, app).await {
+        Ok(_) => {
+            info!("Server stopped gracefully");
+        }
+        Err(error) => {
+            error!("Failed to start server on address:{},{}", ADDR, error);
+        }
+    };
 }

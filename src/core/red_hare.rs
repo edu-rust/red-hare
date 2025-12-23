d550e918149b0885ddb5d5ca41537cb35847cf67
DR.MA <DR.MA@DR.MA>
2025-12-23 22:11:50 +0800

    定义hash类型的数据操作接口

--- src/core/red_hare.rs
+++ src/core/red_hare.rs
@@ -70,16 +70,16 @@ impl RedHare {
             data_type: meta_data.data_type.clone(),
         }))
     }
-
 }
 
 // hash 操作
 impl RedHare {
-    pub fn h_set_string(&self, k: String, v: Vec<u8>) {}
+    pub fn h_set_string(&self, k: String, field: String, v: Vec<u8>) {}
 
-    pub fn h_set_string_with_expire(&self, k: String, v: Vec<u8>) {}
+    pub fn h_set_string_with_expire(&self, k: String, field: String, v: Vec<u8>) {}
 
-    pub fn h_get_with_expire(&self, k: String, v: Vec<u8>) {}
+    pub fn h_get_string(&self, k: String, field: String) {}
+    pub fn h_get_all_string(&self, k: String) {}
 }
 
 //字符串操作

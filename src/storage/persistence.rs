04c61889a1c526ee5494456921e3187a2a070ba6
DR.MA <DR.MA@DR.MA>
2026-02-25 21:36:22 +0800

    代码调整

--- src/storage/persistence.rs
+++ src/storage/persistence.rs
@@ -2,8 +2,9 @@ use crate::config::log::load_config;
 use crate::core::red_hare::{MetaData, RedHare};
 use crate::utils::date::is_after_now;
 use serde::{Deserialize, Serialize};
-use std::fs::File;
+use std::fs::{File, rename};
 use std::io::Write;
+use std::path::Path;
 use tracing::{error, info};
 
 #[derive(Serialize, Deserialize)]
@@ -41,9 +42,16 @@ pub async fn restore_rdb_file() {
 }
 
 pub async fn save_rdb_file() {
+    let log_rdb_path = match load_config() {
+        Ok(config) => config.logging.log_rdb_path,
+        Err(error) => {
+            error!("failed to load_config, error: {}", error);
+            return;
+        }
+    };
     let keys = {
         let red_hare = RedHare::get_instance().lock().await;
-        let keys=red_hare.keys_get();
+        let keys = red_hare.keys_get();
         keys
     };
     if keys.is_empty() {
@@ -63,10 +71,7 @@ pub async fn save_rdb_file() {
                     Ok(is_after_now) => {
                         if is_after_now {
                             //let k1=key;
-                            data_vec.push(Persistence {
-                                key,
-                                meta_data,
-                            });
+                            data_vec.push(Persistence { key, meta_data });
                         }
                     }
                     Err(error) => {
@@ -85,10 +90,10 @@ pub async fn save_rdb_file() {
     if data_vec.is_empty() {
         return;
     }
-    save_key_value_pair(data_vec)
+    save_rdb_rdb_file(data_vec, log_rdb_path)
 }
 
-fn save_key_value_pair(data: Vec<Persistence>) {
+fn save_rdb_rdb_file(data: Vec<Persistence>, log_rdb_path: String) {
     let serial_data = match bincode::serialize(&data) {
         Ok(serial_data) => serial_data,
         Err(error) => {
@@ -99,28 +104,22 @@ fn save_key_value_pair(data: Vec<Persistence>) {
             return;
         }
     };
-    let log_rdb_path = match load_config() {
-        Ok(config) => config.logging.log_rdb_path,
-        Err(error) => {
-            error!("failed to load_config, error: {}", error);
-            return;
-        }
-    };
-    let mut file = match File::create(&log_rdb_path) {
+
+    let path = Path::new(&log_rdb_path);
+    let temp_path = path.with_extension("temp");
+
+    let mut temp_rdb_file = match File::create(&temp_path) {
         Ok(file) => file,
         Err(error) => {
-            error!("failed to create rdb file at {}: {}", log_rdb_path, error);
+            error!(
+                "failed to create log.temp file at {}: {}",
+                "log_rdb.temp", error
+            );
             return;
         }
     };
-    match file.write_all(&serial_data) {
-        Ok(_) => {
-            info!(
-                "successfully wrote rdb file with {} records to {}",
-                data.len(),
-                log_rdb_path
-            );
-        }
+    match temp_rdb_file.write_all(&serial_data) {
+        Ok(_) => {}
         Err(error) => {
             error!(
                 "failed to write rdb file with {} records to {}: {}",
@@ -128,6 +127,25 @@ fn save_key_value_pair(data: Vec<Persistence>) {
                 log_rdb_path,
                 error
             );
+            return;
         }
     }
+    match temp_rdb_file.sync_all() {
+        Ok(_) => {}
+        Err(error) => {
+            error!("failed to sync_all log_rdb__temp_file,error:{}", error);
+            return;
+        }
+    }
+    match rename(temp_path, log_rdb_path) {
+        Ok(_) => {}
+        Err(error) => {
+            error!(
+                "failed to rename from log_rdb_temp_file to log_rdb_file,error:{}",
+                error
+            );
+            return;
+        }
+    };
+    info!("success save_rdb_rdb_file")
 }
